9133964e77de186534453536f993e973537369ac
Andrew Hrechyn <146077376+AndrewHrechyn@users.noreply.github.com>
2025-06-04 22:27:49 +0300

    Update Program.cs

--- licenses/Program.cs
+++ licenses/Program.cs
@@ -1,24 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+// ﻿using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Logging;
 
-namespace licenses
+// namespace licenses
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             CreateWebHostBuilder(args).Build().Run();
+//         }
+
+//         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+//             WebHost.CreateDefaultBuilder(args)
+//                 .UseStartup<Startup>();
+//     }
+// }
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Додаємо сервіси
+builder.Services.Configure<CookiePolicyOptions>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Конфігуруємо middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/LicenseRenewal/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=LicenseRenewal}/{action=Index}/{id?}");
+
+app.Run();
