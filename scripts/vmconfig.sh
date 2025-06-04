18344626a97ea81869fb650076db1c8632822e3a
Andrew Hrechyn <146077376+AndrewHrechyn@users.noreply.github.com>
2025-06-04 22:49:05 +0300

    Update vmconfig.sh

--- scripts/vmconfig.sh
+++ scripts/vmconfig.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Встановлення .NET SDK 8.0
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
@@ -10,11 +9,9 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-8.0
 
-# Клонування актуального репозиторію
 cd /home/azureuser
 git clone https://github.com/AndrewHrechyn/mslearn-load-balance-web-traffic-with-application-gateway.git vehicleapp
 
-# Збірка та публікація веб-додатку
 export DOTNET_CLI_HOME=/home/azureuser
 cd /home/azureuser/vehicleapp/vehicles
 dotnet build
@@ -22,7 +19,6 @@ dotnet publish -o published -c Release
 sudo mkdir -p /opt/vehicleapp
 sudo cp -r published/* /opt/vehicleapp
 
-# Налаштування systemd сервісу
 cat << EOD | sudo tee /etc/systemd/system/vehicleapp.service
 [Unit]
 Description=Vehicle Web App
