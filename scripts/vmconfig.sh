437bfe6007a0c7b30f59c1a9f95da35e4af88927
Andrew Hrechyn <146077376+AndrewHrechyn@users.noreply.github.com>
2025-06-19 10:56:33 +0300

    Update vmconfig.sh

--- scripts/vmconfig.sh
+++ scripts/vmconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
@@ -9,11 +9,11 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-8.0
 
-cd /home/azureuser
+cd /home/azureuser || exit
 git clone https://github.com/AndrewHrechyn/mslearn-load-balance-web-traffic-with-application-gateway.git vehicleapp
 
 export DOTNET_CLI_HOME=/home/azureuser
-cd /home/azureuser/vehicleapp/vehicles
+cd /home/azureuser/vehicleapp/vehicles || exit
 dotnet build
 dotnet publish -o published -c Release
 sudo mkdir -p /opt/vehicleapp
