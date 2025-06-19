9309ab03c5ae38d69fc8a0d7cc840718f35a7c47
Andrew Hrechyn <146077376+AndrewHrechyn@users.noreply.github.com>
2025-06-19 11:05:05 +0300

    Update Program.cs

--- licenses/Program.cs
+++ licenses/Program.cs
@@ -1,65 +1,33 @@
-// ﻿using System;
-// using System.Collections.Generic;
-// using System.IO;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore;
-// using Microsoft.AspNetCore.Hosting;
-// using Microsoft.Extensions.Configuration;
-// using Microsoft.Extensions.Logging;
-
-// namespace licenses
-// {
-//     public class Program
-//     {
-//         public static void Main(string[] args)
-//         {
-//             CreateWebHostBuilder(args).Build().Run();
-//         }
-
-//         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-//             WebHost.CreateDefaultBuilder(args)
-//                 .UseStartup<Startup>();
-//     }
-// }
-
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Додаємо сервіси
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-});
-
-builder.Services.AddControllersWithViews();
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
-// Конфігуруємо middleware
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler("/License/Error");
+    app.UseHsts();
 }
 else
 {
-    app.UseExceptionHandler("/LicenseRenewal/Error");
-    app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseCookiePolicy();
 
 app.UseRouting();
 
+app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=LicenseRenewal}/{action=Index}/{id?}");
+    pattern: "{controller=License}/{action=Index}/{id?}");
 
 app.Run();
